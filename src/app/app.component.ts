a18a70ce15504d78d26da7d1cc686575dbcea7da
ronaldosabino <106283721+ronaldosabino@users.noreply.github.com>
2025-10-17 15:55:19 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Front-End';
+
+  openMenu: boolean = false;
+  toggleMenu() {
+    this.openMenu = !this.openMenu;
+  }
+
+  download() {
+    const link = document.createElement('a');
+    link.href = 'assets/curriculo_Ronaldo_Sabino.pdf';
+    link.download = 'Curriculo_Ronaldo_Sabino.pdf';
+    link.click();
+  }
 }
